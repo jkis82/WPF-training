a9b53fb4085fd14fea95a7961bd21306213b375e
John van der Kist <jkis82@hotmail.com>
2016-03-08 15:15:28 +0100

    Added AdviceSummaryText

--- TestAdvices/TestAdvices/ViewModel/AdviceViewModel.cs
+++ TestAdvices/TestAdvices/ViewModel/AdviceViewModel.cs
@@ -2,6 +2,7 @@
 using System.ComponentModel;
 using System.Globalization;
 using System.Linq;
+using System.Runtime.CompilerServices;
 using System.Threading;
 using System.Windows.Input;
 using TestAdvices.Model;
@@ -9,12 +10,20 @@ using TestAdvices.ViewModel.Commands;
 
 namespace TestAdvices.ViewModel
 {
-   public class AdviceViewModel
+   public class AdviceViewModel : INotifyPropertyChanged
    {
       #region Interface to View
 
+      private string _adviceSummaryText;
+
       public ObservableCollection<VmAdvice> VmAdvices { get; private set; } 
       public ICommand CommitTransitionCommand { get; private set; }
+
+      public string AdviceSummaryText
+      {
+         get { return _adviceSummaryText; }
+         set { _adviceSummaryText = value; OnPropertyChanged(); }
+      }
 
       #endregion
 
@@ -33,6 +42,7 @@ namespace TestAdvices.ViewModel
 
          LoadVmAdvices();
          LoadCommands();
+         UpdateAdviceSummaryText();
       }
 
       private void LoadCommands()
@@ -68,7 +78,30 @@ namespace TestAdvices.ViewModel
       private void NewVmAdviceOnPropertyChanged(object sender, PropertyChangedEventArgs propertyChangedEventArgs)
       {
          if (propertyChangedEventArgs.PropertyName == "State")
+         {
             EvaluateEnableStates();
+            UpdateAdviceSummaryText();
+         }
+      }
+
+      private void UpdateAdviceSummaryText()
+      {
+         int cntActive = VmAdvices.Count(x => x.State == VmAdviceState.Active);
+
+         switch (cntActive)
+         {
+            case 0:
+               AdviceSummaryText = "Er zijn geen adviezen actief";
+               break;
+
+            case 1:
+               AdviceSummaryText = "Er is een advies actief";
+               break;
+
+            default:
+               AdviceSummaryText = string.Format("Er zijn {0} adviezen actief", cntActive);
+               break;
+         }
       }
 
       private void EvaluateEnableStates()
@@ -78,6 +111,13 @@ namespace TestAdvices.ViewModel
 
          foreach (var vmAdvice in VmAdvices)
             vmAdvice.EvaluateEnableState(advicesStarting, advicesStopping);
+      }
+
+      public event PropertyChangedEventHandler PropertyChanged;
+
+      protected virtual void OnPropertyChanged([CallerMemberName] string propertyName = null)
+      {
+         PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
       }
    }
 }
